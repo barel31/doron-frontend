599e7165f6492b271a32c66846f87a2b977e8986
barel31 <barel123133@gmail.com>
2025-01-06 11:57:29 +0200

    chore: remove unnecessary div wrapper from map section

--- components/Footer/MapSection.tsx
+++ components/Footer/MapSection.tsx
@@ -2,10 +2,13 @@ import GoogleMapsEmbed from '../GoogleMapsEmbed';
 
 const MapSection = ({ address }: { address: string }) => (
   <div className="w-full md:w-1/3 space-y-5 text-center">
-    <h4 className="text-xl font-semibold underline underline-offset-8 decoration-lime-500 mb-4">מפה</h4>
-    <div className="w-full h-72 rounded-md overflow-hidden border border-gray-300 shadow-sm">
-      <GoogleMapsEmbed address={address} />
-    </div>
+    <h4 className="text-xl font-semibold underline underline-offset-8 decoration-lime-500 mb-4">
+      מפה
+    </h4>
+    <GoogleMapsEmbed
+      address={address}
+      classNames="w-full h-72 rounded-md overflow-hidden border border-gray-300 shadow-sm"
+    />
   </div>
 );
 
