352423019434a389004f2d046af131bc301bada2
barel31 <barel123133@gmail.com>
2025-09-03 12:58:01 +0300

    fix: add z-index to dropdown content for proper stacking

--- components/Navbar/RenderDropdownContent.tsx
+++ components/Navbar/RenderDropdownContent.tsx
@@ -10,7 +10,7 @@ const RenderDropdownContent = (
   return (
     isDropdownOpen &&
     route.children && (
-      <div className="dropdown-content absolute shadow-lg mt-0 bg-gray-100 dark:bg-gray-700 flex flex-col justify-center rounded-md w-max p-2 m-1 gap-2">
+      <div className="dropdown-content absolute shadow-lg mt-0 bg-gray-100 dark:bg-gray-700 flex flex-col justify-center rounded-md w-max p-2 m-1 gap-2 z-10">
         {route.children.map((childRoute: Route) => (
           <RouteDropdownLink
             route={childRoute}
